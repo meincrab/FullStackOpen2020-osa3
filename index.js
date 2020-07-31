02fa7f895d877f4fd418e7c586c3ad4a4f2520cd
Meincrab <meincrab@gmail.com>
2020-07-31 16:27:43 +0300

    -> 3.7

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const http = require('http')
+const express = require('express')
+const { request } = require('express')
+const app = express()
+app.use(express.json())
+
+
 
 let persons = [
           {
@@ -27,11 +32,62 @@ let persons = [
             "id": 5
           }
         ]
-const app = http.createServer((request, response) => 
-{  response.writeHead(200, { 'Content-Type': 'application/json' }) 
-response.end(JSON.stringify(persons))
+
+
+app.get('/api/persons', (req, res) => {
+  res.send(persons)
 })
 
+
+app.get('/info', (req,res)=>{
+  let date = new Date(new Date().toUTCString())
+  date = JSON.stringify(date)
+  res.write(date)
+  res.write(`\nPhonebook has info for ${persons.length} people`)
+  res.end()
+})
+
+app.get('/api/persons/:id', (request, response)=> {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if(person){
+    response.json(person)
+  }else{
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body) 
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  if(persons.some(person => person.name === body.name)){
+    return response.status(409).json({
+      error: 'already exists, name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.round(Math.random() * 100000)
+  }
+
+  persons = persons.concat(person)
+
+  response.json(persons)
+})
 
 const port = 3001
 app.listen(port)
